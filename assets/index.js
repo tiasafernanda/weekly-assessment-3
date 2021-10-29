7f029a5c844f1e678aa0b25bcb36c490441c7fcc
tiasafernanda <tiasafernanda@gmail.com>
2021-10-29 16:50:34 +0700

    edit

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
